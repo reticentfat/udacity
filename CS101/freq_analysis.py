16bb28a3a3bfcda55efe6c685a95657004116166
reticentfat <reticentfat@users.noreply.github.com>
2017-11-01 15:01:25 +0800

    Create freq_analysis.py

---
+++ CS101/freq_analysis.py
@@ -0,0 +1,49 @@
+# Crypto Analysis: Frequency Analysis
+#
+# To analyze encrypted messages, to find out information about the possible 
+# algorithm or even language of the clear text message, one could perform 
+# frequency analysis. This process could be described as simply counting 
+# the number of times a certain symbol occurs in the given text. 
+# For example:
+# For the text "test" the frequency of 'e' is 1, 's' is 1 and 't' is 2.
+#
+# The input to the function will be an encrypted body of text that only contains 
+# the lowercase letters a-z. 
+# As output you should return a list of the normalized frequency 
+# for each of the letters a-z. 
+# The normalized frequency is simply the number of occurrences, i, 
+# divided by the total number of characters in the message, n.
+
+def freq_analysis(message):
+    ##
+    # Your code here
+    ##
+    freq_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    count = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    y = -1
+    for e in message:
+        x = -1
+        while e != freq_list[x]:
+            x = x + 1
+        if e == freq_list[x]:
+            count[x] = (count[x] + 1)
+    # issues lies within this piece of code
+    while y < (len(count)):
+        y = y + 1
+        count[y] = count[y] / len(message)
+    return count
+
+
+
+#Tests
+
+print freq_analysis("abcd")
+#>>> [0.25, 0.25, 0.25, 0.25, 0.0, ..., 0.0]
+
+print freq_analysis("adca")
+#>>> [0.5, 0.0, 0.25, 0.25, 0.0, ..., 0.0]
+
+print freq_analysis('bewarethebunnies')
+#>>> [0.0625, 0.125, 0.0, 0.0, ..., 0.0]
